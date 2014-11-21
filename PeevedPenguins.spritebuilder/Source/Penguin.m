ddc7885ed4fc0734bf004077bcca5f39ec9a759b
malxlip <malcolmlipscomb@Agent-14.local>
2014-11-20 23:07:35 -0500

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Penguin.m
+++ PeevedPenguins.spritebuilder/Source/Penguin.m
@@ -9,13 +9,6 @@
 #import "Penguin.h"
 
 @implementation Penguin
--(id) init{
-    self = [super init];
-    
-    if (self) {
-        CCLOG(@"Penguin created");
-    }
-    return self;
-}
+
 
 @end
